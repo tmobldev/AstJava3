1dfb1bb8c88b5001a8019f0cf2a4faf14cc40021
tmobldev <tmobldev@gmail.com>
2023-10-09 13:21:12 +0300

    Adding first and second tasks

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,40 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        // Задание №1
+
+        System.out.print("Введите число: ");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        if(num > 7) {
+            System.out.println("Привет");
+        }
+        System.out.println('\n');
+
+
+
+        // Задание №2
+
+        System.out.print("Введите имя: ");
+
+        BufferedReader br2 = new BufferedReader(new InputStreamReader(System.in));
+        String name = br2.readLine();
+        if(name.equals("Вячеслав")) {
+            System.out.println("Привет, Вячеслав");
+        } else {
+            System.out.println("Нет такого имени");
+        }
+        System.out.println('\n');
+
+
+
+        // Задание №3
 
         int[] numbers = new int[30];
         int temp;
